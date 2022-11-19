b0a69ab40ee6c9858f09f3e6f181aa5574208626
YoussefDahab <youssefdahab2009@hotmail.com>
2022-11-18 23:42:36 -0500

    added Bedtime component

--- src/REVITALIZE/screens/sleep-screen.tsx
+++ src/REVITALIZE/screens/sleep-screen.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import { globalStyles } from '../styles/global';
-
+import Bedtime from './Sleep/Bedtime';
 
 const SleepScreen=()=>{
     return (
         <View style={globalStyles.container}>
         <Text style={globalStyles.titleText}>Sleep Screen</Text>
+        <Bedtime />
       </View>
     );
 }
