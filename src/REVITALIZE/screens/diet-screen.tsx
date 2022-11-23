09921e5f56daa2f7f1bb10fc2da03019da4ffd93
Hasankib <53543470+Hasankib@users.noreply.github.com>
2022-11-23 17:30:00 -0500

    stuff

--- src/REVITALIZE/screens/diet-screen.tsx
+++ src/REVITALIZE/screens/diet-screen.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity} from 'react-native';
 import { globalStyles } from '../styles/global';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import moment from 'moment';
 
+const DietScreen=({navigation})=>{
+    const current = new Date();
+    const [dateString, setDateString] = useState(current.toDateString());
+    const [date] = useState(current);
+    
+    function addDate() {
+        date.setDate(date.getDate() + 1);
+        setDateString(date.toDateString());
+    }
 
-const DietScreen=()=>{
+    function subtractDate() {
+        date.setDate(date.getDate() - 1);
+        setDateString(date.toDateString());
+    }
+    
     return (
         <View style={globalStyles.container}>
-        <Text style={globalStyles.titleText}>Diet Screen</Text>
-      </View>
+            <TouchableOpacity style={globalStyles.topCenterContainer}>
+                <Text style={globalStyles.appButtonText}>{ dateString }</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={globalStyles.topLeftContainer}>
+                <Ionicons.Button onPress={()=> subtractDate()} name= 'arrow-back-outline' />
+            </TouchableOpacity>
+            <TouchableOpacity style={globalStyles.topRightContainer}>
+                <Ionicons.Button onPress={()=> addDate()} name= 'arrow-forward-outline' />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>navigation.navigate('Diet Screen')} style={globalStyles.appButtonContainer}>
+                <Text style={globalStyles.appButtonText}>{"Diet Section"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>navigation.navigate('Exercise Screen')} style={globalStyles.appButtonContainer}>
+                <Text style={globalStyles.appButtonText}>{"Exercise Section"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>navigation.navigate('Sleep Screen')} style={globalStyles.appButtonContainer}>
+                <Text style={globalStyles.appButtonText}>{"Sleep Section"}</Text>
+            </TouchableOpacity>
+        </View>
     );
 }
 
