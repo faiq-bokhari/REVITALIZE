587891160ce4fce9d8297270fd79a24cef26e046
YoussefDahab <youssefdahab2009@hotmail.com>
2023-01-28 20:17:48 -0500

    Added styling to login screen

--- src/REVITALIZE/screens/login-screen.tsx
+++ src/REVITALIZE/screens/login-screen.tsx
@@ -5,22 +5,35 @@ import { globalStyles } from '../styles/global';
 const LoginScreen=({navigation})=>{
 
     return (
-        <View style={globalStyles.container}>
+        <View style={globalStyles.loginContainer}>
+            
+            <Text style={globalStyles.welcomeBack}>Welcome Back!</Text>
+            <Text style={globalStyles.bottomPadding}>Please input your information</Text>
 
-        <TextInput
-        style={globalStyles.input}
-        placeholder="Enter name or email"
-        keyboardType="default"
-        />
+            <TextInput
+            style={globalStyles.loginInput}
+            placeholder="Enter name or email"
+            keyboardType="default"
+            />
 
-        <TextInput
-        style={globalStyles.input}
-        placeholder="Enter password"
-        keyboardType="default"
-        />
+            <TextInput
+            style={globalStyles.loginInput}
+            placeholder="Enter password"
+            keyboardType="default"
+            />
 
-        <TouchableOpacity onPress={()=>navigation.navigate('Main Screen')} style={globalStyles.appButtonContainer}>
+            <TouchableOpacity>
+                <Text style={globalStyles.forgotPassword}>{"Forgot Password"}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={()=>navigation.navigate('Main Screen')} style={globalStyles.loginButton}>
                 <Text style={globalStyles.appButtonText}>{"Login"}</Text>
+            </TouchableOpacity>
+
+            <Text>Don't have an account ?</Text>
+
+            <TouchableOpacity>
+                <Text style={globalStyles.forgotPassword}>{"Sign Up"}</Text>
             </TouchableOpacity>
       </View>
     );
