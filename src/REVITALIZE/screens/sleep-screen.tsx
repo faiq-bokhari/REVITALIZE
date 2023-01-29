8e73265650155fb8f09ba5a0e353c3368ff5df53
YoussefDahab <youssefdahab2009@hotmail.com>
2023-01-28 20:18:06 -0500

    Added styling to sleep screen

--- src/REVITALIZE/screens/sleep-screen.tsx
+++ src/REVITALIZE/screens/sleep-screen.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { globalStyles } from '../styles/global';
 import Bedtime from './Sleep/Bedtime';
 
-const SleepScreen=()=>{
+const SleepScreen=({navigation})=>{
     return (
-        <View style={globalStyles.container}>
-        <Text style={globalStyles.titleText}>Sleep Screen</Text>
-        <Bedtime />
+        <View style={globalStyles.loginContainer}>
+          
+          <Text style={globalStyles.sleepText}>Sleep</Text>
+          <Bedtime />
+          <TouchableOpacity onPress={()=>navigation.navigate('Sleep Screen')} style={globalStyles.confirmSleepButton}>
+                <Text style={globalStyles.appButtonText}>{"Confirm Sleep"}</Text>
+            </TouchableOpacity>
       </View>
     );
 }
