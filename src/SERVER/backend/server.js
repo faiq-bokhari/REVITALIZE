241bd7be04ca5178bc0f263cd11bb7afb313d8df
Mahmoud Anklis <anklismahmoud@gmail.com>
2023-01-29 18:15:51 -0500

    Backend setup and DB setup

---
+++ src/SERVER/backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
