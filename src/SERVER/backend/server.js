c73643d352cacd136944ff882bb91ff5b3e4f907
Mahmoud Anklis <anklismahmoud@gmail.com>
2023-01-30 00:05:05 -0500

    Added registering a user

--- src/SERVER/backend/server.js
+++ src/SERVER/backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
+const user = require("./routes/userRoutes");
 const connectDB = require("./config/db");
 
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use("/users", user);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
