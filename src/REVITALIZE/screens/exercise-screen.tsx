254529eca9b77e7a9080f047399f1f988cc651e3
LoganB-81 <loganb8101@gmail.com>
2023-02-11 13:25:51 -0500

    Added styling to exercise page

--- src/REVITALIZE/screens/exercise-screen.tsx
+++ src/REVITALIZE/screens/exercise-screen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { globalStyles } from '../styles/global';
 import AddExercise from './Workout/AddExercise';
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -27,13 +27,21 @@ const ExerciseScreen=({navigation, route})=>{
   function addDate() {
     date.setDate(date.getDate() + 1);
     setDateString(date.toDateString());
-}
+  }
 
-function subtractDate() {
+  function subtractDate() {
     date.setDate(date.getDate() - 1);
     setDateString(date.toDateString());
-}
+  }
+
+  function mybuttonclick() {
+    Alert.alert('hi')
+  }
   
+  const itemSeparator = () => {
+    return <View style={globalStyles.separator}></View>
+  };
+
   return (
       <View style={globalStyles.container}>
             <View style={globalStyles.date_container}>
@@ -47,17 +55,33 @@ function subtractDate() {
                 <Ionicons.Button style={globalStyles.topRightContainer} onPress={()=> addDate()} name= 'arrow-forward-outline' />
             </TouchableOpacity>
             </View>
-        <View style={globalStyles.container}>
-          <Text>
-              Exercise     Reps     Sets     Weight (lbs)
-          </Text>
-          <FlatList 
+        
+        <Text style={globalStyles.listTitle}>Workout Log</Text>
+        <View style={[globalStyles.listContainer,{backgroundColor: '#f9cedf'}]}>
+        <FlatList 
           data={workout}
           renderItem={({ item }) => (
               <View>
                   <Text style={{textAlign: 'left'}}> {item.name}     {item.reps}     {item.sets}     {item.weight}</Text>
               </View>
           )}
+          ItemSeparatorComponent = {itemSeparator}
+          
+          renderItem={({item}) => <View style={globalStyles.listButtonContainer}>
+          <View style={globalStyles.listRowContainer}>
+              <View style={globalStyles.item_Name}>
+              <Text style={globalStyles.item}>{item.name}</Text>
+          </View>
+          <View style={globalStyles.buttonsContainer}>
+          <TouchableOpacity style={globalStyles.list_button}onPress={mybuttonclick}>
+          <Text style={globalStyles.list_button_text}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={globalStyles.list_button}onPress={mybuttonclick}>
+          <Text style={globalStyles.list_button_text}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+        </View>}
           />
       </View>
         <TouchableOpacity onPress={()=>navigation.navigate("Add Exercise Screen")} style={globalStyles.appButtonContainer}>
