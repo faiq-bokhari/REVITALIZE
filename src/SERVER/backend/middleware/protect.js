77cefc33d65f4930ddc25d347e31648e222ef5dc
Mahmoud Anklis <anklismahmoud@gmail.com>
2023-03-07 21:59:28 -0500

    added authentication and password hashing

---
+++ src/SERVER/backend/middleware/protect.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(verifyToken.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res
+        .status(401)
+        .json({ success: false, message: "You are not Authorized" });
+    }
+  }
+
+  if (!token) {
+    res
+      .status(401)
+      .json({ success: false, message: "No token, not authorized" });
+  }
+};
+module.exports = { protect };
--- src/SERVER/backend/routes/userRoutes.js
+++ src/SERVER/backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  getMyInfo,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/protect");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
+router.get("/me", protect, getMyInfo);
 module.exports = router;
