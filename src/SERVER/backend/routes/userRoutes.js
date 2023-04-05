999bcbd56b6437606edd0e5aeffa365877c6dabc
Mahmoud Anklis <anklismahmoud@gmail.com>
2023-04-05 18:51:04 -0400

    Added comments to the user related files, the server file, and the db file

--- src/SERVER/backend/routes/userRoutes.js
+++ src/SERVER/backend/routes/userRoutes.js
@@ -1,5 +1,20 @@
+/*
+Author: Mahmoud Anklis
+Date: January 29 2023
+
+This routing file will be used for the user section of the application.
+
+Overall, this code defines a router object that handles requests related to user data. 
+It defines three different routes for registering, logging in, and getting user information data for a specific user.
+The actual handling of these requests is delegated to separate controller functions, which are imported at the top of the file.
+
+File Association: userModel.js <-> userController.js <-> userRoutes.js
+*/
+
+//import express and and setup express router
 const express = require("express");
 const router = express.Router();
+//import the functions from the userController file and the protect file.
 const {
   registerUser,
   loginUser,
@@ -7,7 +22,11 @@ const {
 } = require("../controllers/userController");
 const { protect } = require("../middleware/protect");
 
+// POST request to register a user
 router.post("/", registerUser);
+// POST request to login
 router.post("/:login", loginUser);
+// GET request to get user information after running the protect middleware.
 router.get("/:me", protect, getMyInfo);
+//Export the router
 module.exports = router;
--- src/SERVER/backend/test/user.test.js
+++ src/SERVER/backend/test/user.test.js
@@ -1,9 +1,23 @@
+/*
+Author: Mahmoud Anklis
+Date: February 5th, 2023
+
+This user.test.js file contains a set of test suites and test cases that aim to test the functionality of the userController.js module. 
+The userController.js module handles requests related to user data, including registering a new user, logging in, and getting user information.
+
+The test cases in this file utilize the Jest testing framework to ensure that the functions in userController.js are working as expected. 
+
+File Association: userModel.js <-> userController.js <-> userRoutes.js
+*/
+
+//imports
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 require("dotenv").config();
 const { registerUser } = require("../controllers/userController");
 
+//Define a test suite for signing up a new user
 describe("Signing up a new User", () => {
   // connect to MongoDB
   beforeAll(async () => {
@@ -14,6 +28,7 @@ describe("Signing up a new User", () => {
   });
   //now = new Date();
 
+  //Reset the user collection in the database and set up mock request, response and next objects before each test
   beforeEach(async () => {
     await User.deleteMany({});
     req = {
